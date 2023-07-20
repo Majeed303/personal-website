5deb68213c86b3d3854663c8475fbf752e1b25d6
majeed <majeedchekkala@gmail.com>
2023-07-20 12:28:28 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["ui/ux | design    |", "Web Development.", "App Development."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true   
+});
